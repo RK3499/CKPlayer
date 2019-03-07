ea20caf9064f274d070f09bab3b4e51c38d583c4
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-03-07 21:27:16 +0530

    Update p6.java

--- p6.java
+++ p6.java
@@ -1,16 +1,14 @@
-    
+import java.lang.*;    
 import java.util.*;
  class Check {
-      public static void main(String[] args) 
-      {
+      public static void main(String[] args){
       int i,n,m=0,k=0;
       String a,b;
       Scanner scan=new Scanner(System.in); 
       a=scan.nextLine(); 
       b=scan.nextLine();
       n=a.length();
-      for(i=0;i<n;i++)
-      {
+      for(i=0;i<n;i++){
           for(int j=i+1;j<n;j++){
           if(a.charAt(i) == a.charAt(j))
           {

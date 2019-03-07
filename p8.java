b352558f16f644e004601b3fec250f66b04fd79b
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-03-07 21:36:38 +0530

    Update p8.java

--- p8.java
+++ p8.java
@@ -6,10 +6,14 @@ class Case
         Scanner in=new Scanner(System.in);
         String sen=in.nextLine().toLowerCase();
         int len=0,i=0;
-        String str[] = sen.split(" ",-2); 
+        //System.out.print(sen);
+        String str[] = sen.split(" ",-2);
+        
+      ///System.out.print(str.length);        
         while(i<str.length)
         { 
           int j=0;
+            // str[i]=str[i].replace(str[i].charAt(0),Character.toUpperCase(str[i].charAt(0)));
             char ch[]=str[i].toCharArray();
             int chlen=ch.length;
             while(j<chlen)
@@ -18,7 +22,8 @@ class Case
               System.out.print(ch[j]);
               j++;
             }
-            System.out.print(" ");             
+            System.out.print(" ");
+          //  System.out.print(chlen);              
         i++;
         }
     }

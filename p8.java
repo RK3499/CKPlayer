1dc939b8d596bb5ec0b561371157f48211822af7
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-03-07 21:34:12 +0530

    Create p8.java

---
+++ p8.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Case
+{
+    public static void main(String ar[])
+     {
+        Scanner in=new Scanner(System.in);
+        String sen=in.nextLine().toLowerCase();
+        int len=0,i=0;
+        String str[] = sen.split(" ",-2); 
+        while(i<str.length)
+        { 
+          int j=0;
+            char ch[]=str[i].toCharArray();
+            int chlen=ch.length;
+            while(j<chlen)
+            {
+              ch[0]=Character.toUpperCase(str[i].charAt(0));
+              System.out.print(ch[j]);
+              j++;
+            }
+            System.out.print(" ");             
+        i++;
+        }
+    }
+}

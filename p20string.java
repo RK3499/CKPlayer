1fbaf01f2cb81d06f9344019a35522fc96dddcd0
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-03-08 21:23:25 +0530

    Update p20string.java

--- p20string.java
+++ p20string.java
@@ -1,7 +1,8 @@
 import java.util.*;
 import java.lang.*; 
-class string{
-	public static void main (String arg[]){ 
+class string5{
+	public static void main (String arg[])
+	{ 
 		Scanner scanf=new Scanner(System.in);
 		String input = scanf.next();
 		for(int i=0;i<input.length();i++)
